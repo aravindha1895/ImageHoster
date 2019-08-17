4c5c020754bb9e0a3f295d2832ab5e735d8ce9b5
aravindha1895 <sridharanrvnd@gmail.com>
2019-08-17 18:40:03 +0530

    Assessment 2 - Display images

--- src/main/java/ImageHoster/controller/HomeController.java
+++ src/main/java/ImageHoster/controller/HomeController.java
@@ -7,19 +7,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ImageHoster.model.Image;
+import ImageHoster.service.ImageService;
+
 
 @Controller
 public class HomeController {
 
+	@Autowired
+	ImageService imageservice;
+	
     public HomeController() {
         System.out.println("*** HomeController ***");
     }
 
     @RequestMapping("/")
-    public String homePage(Model model) {
-
+    public String getAllImages(Model model) {
+        //Call getAllImages() method in ImageService class to get the list of all images
+        //Add the list of images in the model with the key as "images"
+    	List<Image> images =imageservice.getAllImages();
+    	model.addAttribute("images", images);
         return "index";
-
     }
 
 }
