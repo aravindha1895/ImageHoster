9cdf43863b9b7f7f2eb05e04845e8dd358bdc2d4
aravindha1895 <sridharanrvnd@gmail.com>
2019-09-07 23:59:22 +0530

    Assessment  Tags

--- src/main/java/ImageHoster/model/Image.java
+++ src/main/java/ImageHoster/model/Image.java
@@ -1,22 +1,24 @@
 package ImageHoster.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
-//Write the annotation to specify that the corresponding class is a JPA entity
-//Write the annotation to provide more options to customize the mapping.
-//Here the name of the table to be created in the database is to be explicitly mentioned as 'images'. Hence the table named 'images' will be created in the database with all the columns mapped to all the attributes in 'Image' class
+//@Entity annotation specifies that the corresponding class is a JPA entity
 @Entity
-@Table(name="images")
+//@Table annotation provides more options to customize the mapping.
+//Here the name of the table to be created in the database is explicitly mentioned as 'images'. Hence the table named 'images' will be created in the database with all the columns mapped to all the attributes in 'Image' class
+@Table(name = "images")
 public class Image {
 
-    //Write the annotation to specify that the attribute is a primary key
-    //Write the annotation to specify that the Generation type is AUTO
-    //Also write the annotation to map this attribute to a column in the database and also explicitly specify the column name as 'id'
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name="id")
+    //@Id annotation specifies that the corresponding attribute is a primary key
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@Column annotation specifies that the attribute will be mapped to the column in the database.
+    //Here the column name is explicitly mentioned as 'id'
+    @Column(name = "id")
     private Integer id;
 
     @Column(name = "title")
@@ -38,12 +40,16 @@ public class Image {
     //The 'images' table is mapped to 'users' table with Many:One mapping
     //One image can have only one user (owner) but one user can have multiple images
     //FetchType is EAGER
-    //Write the annotation to implement the above feature
-    //Write the annotation to indicate that the name of the column in 'images' table referring the primary key in 'users' table will be 'user_id'
-    
-    @ManyToOne(fetch= FetchType.EAGER)
-    @JoinColumn(name="user_id")
+    @ManyToOne(fetch = FetchType.EAGER)
+    //Below annotation indicates that the name of the column in 'images' table referring the primary key in 'users' table will be 'user_id'
+    @JoinColumn(name = "user_id")
     private User user;
+
+    //The attribute contains a list of all the tags of an image
+    //Note that no column will be generated for this attribute in the database instead a new table will be created
+    //Since the mapping is Many to Many, a new table will be generated containing the two columns both referencing to the primary key of both the tables ('images', 'tags')
+    @ManyToMany(fetch = FetchType.LAZY)
+    private List<Tag> tags = new ArrayList<>();
 
     public Image() {
     }
@@ -110,5 +116,13 @@ public class Image {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
     }
 }
